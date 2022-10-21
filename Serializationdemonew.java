fe87d602451e20d37194dbbe7d03342b0d7c6aec
Vidisha5 <74805512+Vidisha5@users.noreply.github.com>
2022-10-21 23:32:22 +0530

    Update Serializationdemonew.java

--- Serializationdemonew.java
+++ Serializationdemonew.java
@@ -8,7 +8,7 @@ public class Serializationdemonew {
 	public static void main(String[] args) throws IOException {
 		
 		try {
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("file.txt"));
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("file.txt"));//Exception in thread "main" java.io.InvalidClassException: Cat232; local class incompatible: stream classdesc serialVersionUID = 1, local class serialVersionUID = 2
 		
 			Cat232 cat1 = (Cat232)ois.readObject();
 			
